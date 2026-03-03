04aebb05efa97cfdbc53243b60735c8a272ed96d
Aditya Nadamuni <aditya.nadamuni@gmail.com>
2026-03-04 00:27:33 +0530

    Refactor code structure for improved readability and maintainability

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,9 @@
 import NextAuth from "next-auth"
 import { authConfig } from "@/lib/auth.config"
 
-export const { auth: middleware } = NextAuth(authConfig)
-export default middleware
+const { auth } = NextAuth(authConfig)
+
+export default auth
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|icons).*)"],
